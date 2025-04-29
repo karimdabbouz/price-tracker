a3e173f531140ae6d80a3fbee98a2920df0e2d2b
karimdabbouz <hey@karim.ooo>
2025-04-29 20:30:10 +0200

    add release year filter

--- ic/src/ic_frontend/src/routes/marken/[manufacturer]/+page.svelte
+++ ic/src/ic_frontend/src/routes/marken/[manufacturer]/+page.svelte
@@ -5,6 +5,8 @@
     import { page } from "$app/stores";
     import { manufacturers } from "$lib/stores";
     import { API_URL } from "$lib/config";
+    import AutoComplete from "simple-svelte-autocomplete";
+    
 
     // state
     let manufacturer: string | undefined;
@@ -17,6 +19,12 @@
         total_count: 0
     }
 
+    // Helper to get all years from 2020 to max in state.release_year
+    $: availableYears = (() => {
+        const maxYear = Math.max(...state.release_year, 2020);
+        return Array.from({ length: maxYear - 2020 + 1 }, (_, i) => 2020 + i);
+    })();
+
     // load function
     const loadProducts = async () => {
         state.loading = true;
@@ -28,7 +36,6 @@
                 filterParams.append(key, String(value));
             }
         });
-        console.log(filterParams.toString());
         const response = await fetch(
             `${API_URL}/manufacturers/${$page.params.manufacturer}/product_listings?${filterParams}`
         );
@@ -47,6 +54,8 @@
         state.offset += state.limit;
         await loadProducts();
     }
+
+    // handle set release year
 
     // set manufacturer name from url param
     onMount(() => {
@@ -81,19 +90,10 @@
     </div>
 
     <div class="flex items-center justify-between my-4">
-        <div class="flex bg-blue-500">
+        <div class="flex">
             <div>
-                Sortieren nach: Veröffentlichungsjahr, Preis
+                <AutoComplete items={availableYears} bind:selectedItem={state.release_year} placeholder="Veröffentlicht in..." maxItemsToShowInList={5} multiple={true}/>
             </div>
-            <div>
-                Sort direction arrow
-            </div>
-            <div>
-                Checkbox: Angebote mit mehreren Preisen zuerst anzeigen
-            </div>
-        </div>
-        <div class="bg-red-500">
-            ende
         </div>
     </div>
 
