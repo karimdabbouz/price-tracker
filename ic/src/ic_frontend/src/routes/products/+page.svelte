8ba1977bafbd25b51d156e4ea201b4fd534bad40
karimdabbouz <hey@karim.ooo>
2025-05-03 20:22:45 +0200

    add product page

---
+++ ic/src/ic_frontend/src/routes/products/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "../../index.css";
+    import Navbar from "../../components/Navbar.svelte";
+</script>
+
+<Navbar />
+
+<div class="flex flex-col min-h-screen mx-20">
+
+    products page
+
+</div>
---
+++ ic/src/ic_frontend/src/routes/products/[id]-[manufacturer]-[manufacturer_id]-[name]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import "../../../index.css";
+    $: ({ id, manufacturer, manufacturer_id, name } = $page.params);
+</script>
+
+
+single product page hello
+
+{id}
+{manufacturer}
+{manufacturer_id}
+{name}
